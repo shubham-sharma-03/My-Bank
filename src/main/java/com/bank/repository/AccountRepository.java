cf37cde45fde6b90fb7877b3e6e7fe8a7c209537
Shubham Sharma <sharmashubham05275@gmail.com>
2026-04-15 16:19:58 +0530

    Remove unnecessary blank lines in AccountRepository

--- src/main/java/com/bank/repository/AccountRepository.java
+++ src/main/java/com/bank/repository/AccountRepository.java
@@ -5,13 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-
 public interface AccountRepository extends JpaRepository<Account, Long> {
     Optional<Account> findByAccountNumber(String accountNumber);
 
     
     List<Account> findByUserId(Long userId);
     void deleteByAccountNumber(String accountNumber);
-
 
 }
