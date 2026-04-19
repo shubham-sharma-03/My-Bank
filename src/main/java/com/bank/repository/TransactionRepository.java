35950bdec575a89c7d2042d15dfb22ec012285e7
Shubham Sharma <sharmashubham05275@gmail.com>
2026-04-19 12:53:18 +0530

    Add method to find transactions by multiple accounts

--- src/main/java/com/bank/repository/TransactionRepository.java
+++ src/main/java/com/bank/repository/TransactionRepository.java
@@ -2,8 +2,10 @@ package com.bank.repository;
 
 import com.bank.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
     List<Transaction> findBySenderAccountOrReceiverAccount(String sender, String receiver);
+    List<Transaction> findBySenderAccountInOrReceiverAccountIn(List<String> sender, List<String> receiver);
 }
