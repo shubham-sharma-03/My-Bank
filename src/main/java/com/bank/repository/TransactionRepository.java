1349df44db19af9ce39725fd9ea281a028758bb3
Shubham Sharma <sharmashubham05275@gmail.com>
2026-04-24 13:06:00 +0530

    Remove unnecessary blank line in TransactionRepository

--- src/main/java/com/bank/repository/TransactionRepository.java
+++ src/main/java/com/bank/repository/TransactionRepository.java
@@ -2,7 +2,6 @@ package com.bank.repository;
 
 import com.bank.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
