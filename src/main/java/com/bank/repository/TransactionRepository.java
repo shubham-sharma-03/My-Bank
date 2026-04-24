c728559330a6c7e8a37a42e396d272816db5aeb5
Shubham Sharma <sharmashubham05275@gmail.com>
2026-04-24 13:05:31 +0530

    Add methods to find transactions by accounts

--- src/main/java/com/bank/repository/TransactionRepository.java
+++ src/main/java/com/bank/repository/TransactionRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    
     List<Transaction> findBySenderAccountOrReceiverAccount(String sender, String receiver);
     List<Transaction> findBySenderAccountInOrReceiverAccountIn(List<String> sender, List<String> receiver);
     
