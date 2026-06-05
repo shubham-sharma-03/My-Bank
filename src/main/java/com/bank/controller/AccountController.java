f993b995d9d86e2a15baabf9633063f1e5941cd4
Shubham Sharma <sharmashubham05275@gmail.com>
2026-06-05 20:33:30 +0530

    Remove unused import statement in AccountController

--- src/main/java/com/bank/controller/AccountController.java
+++ src/main/java/com/bank/controller/AccountController.java
@@ -8,7 +8,6 @@ import com.bank.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
