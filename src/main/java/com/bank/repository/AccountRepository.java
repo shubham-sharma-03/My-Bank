5f4d8d6ac9467e8c23cdf7c23862425ec265d943
Shubham Sharma <sharmashubham05275@gmail.com>
2026-06-10 08:15:09 +0530

    Add methods to find and delete accounts by number

--- src/main/java/com/bank/repository/AccountRepository.java
+++ src/main/java/com/bank/repository/AccountRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    
     Optional<Account> findByAccountNumber(String accountNumber);
 
     List<Account> findByUserId(Long userId);
