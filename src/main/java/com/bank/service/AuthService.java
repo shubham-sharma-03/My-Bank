5ca81d96b5effc8b47de732431c3553204856c89
Shubham Sharma <sharmashubham05275@gmail.com>
2026-06-22 17:20:22 +0530

    Format AuthService class declaration

--- src/main/java/com/bank/service/AuthService.java
+++ src/main/java/com/bank/service/AuthService.java
@@ -8,7 +8,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AuthService {
+public class AuthService
+    {
 
     @Autowired
     private UserRepository userRepository;
